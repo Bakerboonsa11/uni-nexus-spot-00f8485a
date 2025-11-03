67b358a9c7beae3cdf83c978542c05116be9e203
Bonsa Beker <bakerboonsa@gmail.com>
2025-11-03 09:27:17 +0300

    app fix

--- public/sw.js
+++ public/sw.js
@@ -1,30 +1,14 @@
-const CACHE_NAME = 'nexus-spot-v1';
-const urlsToCache = [
-  '/',
-  '/home',
-  '/services',
-  '/market',
-  '/jobs',
-  '/dashboard',
-  '/auth'
-];
+const CACHE_NAME = 'nexus-spot-v2';
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+  // Simple fetch handler
+  event.respondWith(fetch(event.request));
 });
